3489cec42cddc0a1ac47ee1a36b9c3225a9a4912
tonchan1216 <viva.la.vida27315@gmail.com>
2017-08-12 02:58:36 +0900

    answer

--- app/main.py
+++ app/main.py
@@ -4,6 +4,43 @@ def main(argv):
   # ---
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
-
   for i, v in enumerate(argv):
-    print("argv[{0}]: {1}".format(i, v))
+    print(look_and_say(int(v))[:500])
+
+#秘密の規則にそった数列を生成する関数
+def look_and_say(num):
+	temp = ''
+
+	for i in range(1, num+1):
+		if i == 1:
+			temp = '1'
+		else:
+			temp = translate_sequence(temp)
+	return temp
+
+#入力文字列をルールに従って変換
+def translate_sequence(target_sequence):
+	target_list = []
+	answer_sequence = ''
+
+	while len(target_sequence) > 0:
+		if len(target_sequence) == 1:
+			target_list.append(target_sequence)
+			break
+
+		else:
+			i = target_sequence[0]
+			slice_index = 1
+			for j in target_sequence[1:]:
+				if i == j:
+					slice_index += 1
+				else:
+					break
+			
+			target_list.append(target_sequence[:slice_index])
+			target_sequence = target_sequence[slice_index:]
+
+
+	for x in target_list:
+		answer_sequence += str(x.count(x[0])) + x[0]
+	return answer_sequence[:750]
